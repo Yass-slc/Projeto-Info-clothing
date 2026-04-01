ef096e3cc705d8c242bb95be572bb41e911de047
Ana Julia Paulino Ribeiro <00001124306080sp@aluno.educacao.sp.gov.br>
2026-04-01 20:49:24 -0300

    criando o servidor

--- front-end/codigo.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const botao = document.getElementById("btnCavalo");
-
-    botao.addEventListener("click", function() {
-        alert("O cavalo galopou!");
-        
-        // Adicione aqui a ação que o botão deve realizar
-    });
-});
